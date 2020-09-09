ff3017a7338129f0ffb1f6735e6c2e3c741a36f8
John Wells <molekewl2017@gmail.com>
2020-09-09 12:19:59 -0700

    click on progress bar to scroll/skip through the video

--- script.js
+++ script.js
@@ -47,6 +47,15 @@ function updateProgress() {
   duration.textContent = `${displayTime(video.duration)}`;
 }
 
+// Click to seek within the video
+function setProgress(e) {
+  // offsetX is where you clicked / offsetWidth is the length of the progressbar
+  const newTime = e.offsetX / progressRange.offsetWidth;
+  progressBar.style.width = `${newTime * 100}%`;
+  video.currentTime = newTime * video.duration;
+  console.log(newTime);
+}
+
 // Volume Controls --------------------------- //
 
 
@@ -62,3 +71,4 @@ playBtn.addEventListener('click', togglePlay);
 video.addEventListener('click', togglePlay);
 video.addEventListener('timeupdate', updateProgress);
 video.addEventListener('canplay', updateProgress);
+progressRange.addEventListener('click', setProgress);
