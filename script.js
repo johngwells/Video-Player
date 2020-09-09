de637fd14d2f5f97191658384e822b5204dc262f
John Wells <molekewl2017@gmail.com>
2020-09-09 12:56:30 -0700

    playback speed

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ const volumeRange = document.querySelector('.volume-range');
 const volumeBar = document.querySelector('.volume-bar');
 const currentTime = document.querySelector('.time-elapsed');
 const duration = document.querySelector('.time-duration');
+const speed = document.querySelector('.player-speed');
 const fullscreenBtn = document.querySelector('.fullscreen');
 
 // Play & Pause ----------------------------------- //
@@ -103,7 +104,11 @@ function toggleMute() {
 }
 
 // Change Playback Speed -------------------- //
-
+function changeSpeed() {
+  console.log('video playback rate', video.playbackRate);
+  console.log('selected value', speed.value);
+  video.playbackRate = speed.value;
+}
 
 
 // Fullscreen ------------------------------- //
@@ -116,3 +121,4 @@ video.addEventListener('canplay', updateProgress);
 progressRange.addEventListener('click', setProgress);
 volumeRange.addEventListener('click', changeVolume);
 volumeIcon.addEventListener('click', toggleMute);
+speed.addEventListener('change', changeSpeed);
